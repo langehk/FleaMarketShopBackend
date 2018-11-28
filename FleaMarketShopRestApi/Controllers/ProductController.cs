1d4595a76d7f33810ea39b5dbeaaffecbe2f5c2a
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-11-28 13:16:38 +0100

    clean up

--- FleaMarketShopRestApi/Controllers/ProductController.cs
+++ FleaMarketShopRestApi/Controllers/ProductController.cs
@@ -6,8 +6,6 @@ using FleaMarketShop.Core.ApplicationService;
 using FleaMarketShop.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace FleaMarketShopRestApi.Controllers
 {
     [Route("api/[controller]")]
